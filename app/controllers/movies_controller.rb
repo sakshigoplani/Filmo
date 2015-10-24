be20cd389b78586fe80255f7f7c6f7643e2e6c8a
Ubuntu Final <pallavikotkat17@gmail.com>
2015-10-23 22:11:08 -0700

    Review part-2 with star ratings

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,7 +12,13 @@ before_action :authenticate_user!, except: [:index, :show]
   # GET /movies/1.json
   def show
 @reviews=Review.where(movie_id: @movie.id).order("created_at DESC")
-  end
+
+if @reviews.blank?
+@avg_review=0
+else
+@avg_review=@reviews.average(:rating).round(2)
+end
+ end
 
   # GET /movies/new
   def new
