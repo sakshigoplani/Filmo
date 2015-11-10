8587a6bb3e0b97d6800a19bb018564b3ef68d841
salilkanetkar <salil.kanetkar@gmail.com>
2015-11-09 16:49:31 -0800

    IMDB_Above up fixed

--- app/models/search.rb
+++ app/models/search.rb
@@ -5,7 +5,7 @@ class Search < ActiveRecord::Base
     movies= movies.where(["genres LIKE ?" , genres]) if genres.present?
     movies= movies.where(["price >= ?",min_price]) if min_price.present?
     movies= movies.where(["price <= ?",max_price]) if max_price.present?
-    movies= movies.where(["imdb_rating LIKE ?",imdb_above]) if imdb_above.present?
+    movies= movies.where(["imdb_rating >= ?",imdb_above]) if imdb_above.present?
   return movies
 end
 end
