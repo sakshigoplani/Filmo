01c6efa8db21fb29a80ef0c002ddecdca48d517f
Salil Ubuntu <salil.kanetkar@gmail.com>
2015-11-23 00:46:36 -0800

    Testing Results

--- db/migrate/20151022095234_create_movies.rb
+++ db/migrate/20151022095234_create_movies.rb
@@ -1,5 +1,6 @@
 class CreateMovies < ActiveRecord::Migration
   def change
+    #add_index :movies, :title
     create_table :movies do |t|
       t.string :title
       t.string :poster_url
@@ -9,7 +10,6 @@ class CreateMovies < ActiveRecord::Migration
       t.decimal :length
       t.string :studio
       t.decimal :price
-
       t.timestamps null: false
     end
   end
---
+++ load_tests/GraphPlotsTsung/Tsung_without_optimizations_C3/.bash_history
@@ -0,0 +1,18 @@
+less /var/log/cloud-init-output.log 
+exit
+sudo cpan Template
+exit
+ls
+ll
+exit
+ll
+http://ec2-52-11-218-71.us-west-2.compute.amazonaws.com
+Chat Conversation End
+ll
+quit
+exit
+tsung -f filmo_critical_final.xml start
+cd /home/ec2-user/.tsung/log/20151120-2214
+tsung_stats.pl
+cd Downloads
+exit
