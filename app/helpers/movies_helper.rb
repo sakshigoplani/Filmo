0e86930e8a2c51d7732ff068201a9560dc33a85f
Sakshi Goplani <sakshigoplani9@gmail.com>
2015-11-24 01:23:43 -0800

    Updated dalli

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,14 @@
 module MoviesHelper
+
+
+def cache_key_for_movie_table
+ "movie-table-#{Movie.maximum(:updated_at)}"
+end
+
+def cache_key_for_movie_row(movie)
+ "movie-#{movie.id}-#{movie.updated_at}"
+end
+
+
+
 end
