db17dc462bfa39079b9721faa3d6646e0ae7d31b
Sakshi Goplani <sakshigoplani9@gmail.com>
2015-11-24 01:21:12 -0800

    Updated dalli

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,8 +13,8 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = false
-
+  config.action_controller.perform_caching = true
+  config.cache_store = :dalli_store
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
