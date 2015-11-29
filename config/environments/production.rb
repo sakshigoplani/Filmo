ce2b855fc354039fd31c9fa5028e2769b8164dfc
Sakshi Goplani <sakshigoplani9@gmail.com>
2015-11-29 15:58:22 -0800

    Updated config cache classes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = false
+  config.cache_classes = true
   #to add static files
   config.serve_static_assets = true
   # Eager load code on boot. This eager loads most of Rails and
