9b738b5374cbd86901dcdd191018c5534e94396f
Pallavi Kotkar <pallavi24@users.noreply.github.com>
2015-11-30 01:29:40 -0800

    paginated

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,17 +13,17 @@ before_action :authenticate_user!, except: [:index, :show]
   def index
   @flag = 1
   @movies = Movie.all
-  #@movies = Movie.paginate(:page => params[:page], :per_page => 10)
+  @movies = Movie.paginate(:page => params[:page], :per_page => 10)
 
    if params[:search]
 	if(Movie.search(params[:search]).blank?)
 	  @flag = 0
           @movies = Movie.all
-	  #@movies = @movies.paginate(:page => params[:page], :per_page => 10)
+	  @movies = @movies.paginate(:page => params[:page], :per_page => 10)
 	else
 	  @flag = 1
 	  @movies = Movie.search(params[:search])#.order("created_at DESC")
-	  #@movies = @movies.paginate(:page => params[:page], :per_page => 10)
+	  @movies = @movies.paginate(:page => params[:page], :per_page => 10)
   # elsif(Movie.search(params[:search]).blank?)
 #	@movies = Movie.all
 	#puts "Hii"
