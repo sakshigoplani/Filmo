2af7af839f6a8335a97b9881767e47f66afb5c68
Sakshi Goplani <sakshigoplani9@gmail.com>
2015-11-30 22:05:21 -0800

    Updated memcache

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,5 +1,12 @@
 module MoviesHelper
-   def cache_key_for_movie(movie)
-    "movie-index-#{movie.id}-#{movie.updated_at}"
-   end
+
+def cache_key_for_movie_row(movie)
+ "movies-movie-#{movie.id}-#{movie.updated_at}"
+end
+
+def cache_key_for_movie_table
+ max_updated_at = @movies.max_by(&:updated_at).updated_at.try(:utc).try(:to_s, :number)
+ "movie-table-#{Movie.maximum(:updated_at)}"
+end
+
 end
