a481ed91568ffec92e141afdc96f2ce5e6197a76
Sakshi Goplani <sakshigoplani9@gmail.com>
2015-12-01 15:24:17 -0800

    Removed caching

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,12 +1,2 @@
 module MoviesHelper
-
-def cache_key_for_movie_row(movie)
- "movies-movie-#{movie.id}-#{movie.updated_at}"
-end
-
-def cache_key_for_movie_table
- max_updated_at = @movies.max_by(&:updated_at).updated_at.try(:utc).try(:to_s, :number)
- "movie-table-#{Movie.maximum(:updated_at)}"
-end
-
 end
