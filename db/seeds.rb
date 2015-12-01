0cc4419e64e091e3fe0e043002740d2b5db8b2bd
Salil Ubuntu <salil.kanetkar@gmail.com>
2015-12-01 15:47:55 -0800

    Seeding Extra Large Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,7 +28,7 @@ if mutex
 	u.save!;
 end
 
-1.upto(10000) do |i|
+1.upto(100000) do |i|
 	if ((i % 2) == 0)
 		p = Post.create!(title: "Post #{i}",
 						content: 'Content of Post #{i}',
@@ -184,7 +184,7 @@ end
 	end
 end
 
-1.upto(10000) do |i|
+1.upto(100000) do |i|
 	c = Comment.create!(comment: 'Comment for the above post',
 				post_id: "#{i}",
 				user_id: '1',
@@ -192,11 +192,11 @@ end
 				updated_at: '10/10/2015 17:00');
 	c.save!;
 
-	c1 = Comment.create!(comment: 'Comment for the above post',
-				post_id: "#{i}",
-				user_id: '1',
-				created_at: '10/11/2015 17:00',
-				updated_at: '10/11/2015 17:00');
-	c1.save!;
+	#c1 = Comment.create!(comment: 'Comment for the above post',
+	#			post_id: "#{i}",
+	#			user_id: '1',
+	#			created_at: '10/11/2015 17:00',
+	#			updated_at: '10/11/2015 17:00');
+	#c1.save!;
 end
 end
--- lib/tasks/my_rakes.rake
+++ lib/tasks/my_rakes.rake
@@ -1,6 +1,6 @@
 desc "Import movies."
   task :import_movies => :environment do
-    File.open("Dataset/Movie_Dataset_10K.csv", "r").each do |line|
+    File.open("Dataset/Movie_Dataset_1Lakh.csv", "r").each do |line|
       title , poster_url , genres , year , imdb_rating , length, studio , price = line.strip.split(",")
       m = Movie.new(:title => title, :poster_url => poster_url, :genres => genres , :year => year , :imdb_rating => imdb_rating , :length => length , :studio => studio , :price => price)
       m.save
